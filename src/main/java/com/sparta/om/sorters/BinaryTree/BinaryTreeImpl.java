7fd78117daad05b8d21c4ddbd2b7b1cc35ae0fbe
Omer Minhas <omerminhas1@gmail.com>
2022-08-12 16:20:26 +0100

    moved Node into BinaryTreeImpl as a nested class

--- src/main/java/com/sparta/om/sorters/BinaryTree/BinaryTreeImpl.java
+++ src/main/java/com/sparta/om/sorters/BinaryTree/BinaryTreeImpl.java
@@ -28,6 +28,42 @@ public class BinaryTreeImpl implements BinaryTree{
         addNodeToTree(rootNode, element);
     }
 
+    public class Node {
+        private final int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(Node leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public Node getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(Node rightChild) {
+            this.rightChild = rightChild;
+        }
+        public boolean isLeftChildEmpty() {
+            return leftChild == null;
+        }
+        public boolean isRightChildEmpty() {
+            return rightChild == null;
+        }
+    }
+
     @Override
     public void addElements(int[] elements) {
         for (int i = 0; i < elements.length; i++) {
